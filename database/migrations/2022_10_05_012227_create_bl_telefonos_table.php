20e4c29005fe79bbcd3a5cdb4ad5494371fe788c
Arnaldo Bonillo <bonillaarnaldo@gmail.com>
2022-10-04 21:39:00 -0400

    creacion de los nuevos migrations

---
+++ database/migrations/2022_10_05_012227_create_bl_telefonos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlTelefonosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bl_telefonos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bl_telefonos');
+    }
+}
